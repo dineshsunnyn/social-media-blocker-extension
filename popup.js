ceacd1caafe5795c36b188d58ff19baf28a9590c
dinesh sunny <dineshsunnynarsinga@gmail.com>
2025-07-07 23:58:53 -0500

    first version - can add websites dynamically

--- popup.js
+++ popup.js
@@ -1,29 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("blockForm");
-  const blockedWebsitesList = document.getElementById("blockedWebsites");
-  
+  const blockedWebsitesList = document.getElementById("blockedSitesList");
+  const new_website = document.getElementById("websiteInput");
 
+    let new_id_start = Math.floor(Date.now())%1000000;
   form.addEventListener("submit", function (event) {
     event.preventDefault();
+    const website = new_website.value.trim();
 
-    const websiteInput = form.elements["website"];
-    const website = websiteInput.value.trim();
-
+    
     if (website) {
+      const new_rule = {
+        id: new_id_start,
+        priority: 1,
+        action: {
+          type: "block",
+        },
+        condition: {
+          urlFilter: website,
+          resourceTypes: ["main_frame"],
+        },
+      };
+
+    //   new_website.value="";
+
+      chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: [new_rule],
+        removeRuleIds: [],
+      });
+
       // Display the website on the popup UI
       const li = document.createElement("li");
       li.textContent = website;
       blockedWebsitesList.appendChild(li);
 
       // Clear the input field
-      websiteInput.value = "";
+      new_website.value = "";
 
       // Fetch existing blocked URLs from storage
       chrome.storage.local.get("blockedURLS", (result) => {
         const currentList = result.blockedURLS || [];
 
         // Add the new website
-        currentList.push(website);
+        currentList.push({ website, id: new_rule.id });
 
         // Update storage
         chrome.storage.local.set({ blockedURLS: currentList });
